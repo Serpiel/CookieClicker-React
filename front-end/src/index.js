1958df453bbaa2c444eab7b7e75f2479a078a1b2
Serpiel <hmarujo1510@gmail.com>
2026-02-12 13:08:34 +0100

    Init of the project

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './/components/styles/index.css';
+import App from './/components/layouts/App';
+import reportWebVitals from './/components/features/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
